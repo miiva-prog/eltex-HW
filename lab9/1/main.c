d5897888cfa2e5acf24bf3d9ef38dd7c4debc395
Ivan <ivan.mikhalov.1104@gmail.com>
2025-03-10 11:41:49 +0700

    lab9

---
+++ lab9/1/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define SIZE 20
+
+int main(int argc, char **argv) {
+  FILE *file = fopen(argv[1], "w");
+
+  if (file == NULL) {
+    printf("can't open file\n");
+    return -1;
+  }
+
+  char arr[SIZE];
+
+  fprintf(file, "%s", "String from file");
+  fclose(file);
+
+  file = fopen(argv[1], "r");
+
+  if (file == NULL) {
+    printf("can't open file\n");
+    return -1;
+  }
+
+  fgets(arr, SIZE, file);
+  printf("%s\n", arr);
+
+  return 0;
+}
